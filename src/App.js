a8ac1f7078805798e864c6d41dcc139a945942df
Felixx-jnr <uyuoukoh@gmail.com>
2024-05-07 12:16:06 +0100

    Initial routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import "./scss/index.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import House from "./pages/House";
+import HouseDetails from "./pages/HouseDetails";
+import Notfound from "./pages/Notfound";
 
 function App() {
   return (
-    <h1 className="text-5xl font-bold underline py-8 px-7 bg-red-500 text-green-800 ">
-      Hello world!
-    </h1>
+    <Router>
+      <div className="App">
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/contact"
+              element={<Contact />}
+            />
+            <Route
+              path="/about"
+              element={<About />}
+            />
+            <Route
+              path="/house"
+              element={<House />}
+            />
+            <Route
+              path="/house/house-details/:id"
+              element={<HouseDetails />}
+            />
+            <Route
+              path="*"
+              element={<Notfound />}
+            />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
