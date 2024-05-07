0213ced73d929e93e46163742a2ab283273efabe
Felixx-jnr <uyuoukoh@gmail.com>
2024-05-07 07:07:37 +0100

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./scss/App.scss";
+import "./scss/index.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <h1 className="text-5xl font-bold underline py-8 px-7 bg-red-500 text-green-800 ">
+      Hello world!
+    </h1>
+  );
 }
 
 export default App;
