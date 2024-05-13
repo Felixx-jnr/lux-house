6643a49ed8ca34f19061bf981496587d628ebd37
Felixx-jnr <uyuoukoh@gmail.com>
2024-05-13 16:18:55 +0100

    Reviews Modal begin

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,7 +3,59 @@ import house from "../asset/house.jpeg";
 
 const Grid = () => {
   return (
-    <main className=" ">
+    <main className="my-40">
+      <section className="section-one">
+        <div className="relative writeup ">
+          <p className="absolute -top-3 left-0 bg-background px-3 font-bold ">
+            Engineering Expertise
+          </p>
+
+          <div className="one">
+            <h1 className=" text-[800%] leading-none font-bold ">01</h1>
+
+            <p className="pt-4 font-semibold text-justify p-2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+              harum minus, fuga esse reiciendis accusantium ad quam molestias
+              dignissimos soluta? Quasi soluta nulla quam sint expedita maxime
+              aliquid quo aut, similique
+            </p>
+          </div>
+        </div>
+
+        <img
+          className="pic"
+          src={house}
+          alt=""
+          style={{ width: "100%" }}
+        />
+      </section>
+
+      <section className="section-two">
+        <img
+          className="pic"
+          src={house}
+          alt=""
+          style={{ width: "100%" }}
+        />
+
+        <div className="relative writeup ">
+          <p className="absolute -top-3 left-0 bg-background px-3 font-bold ">
+            Engineering Expertise
+          </p>
+
+          <div className="two">
+            <p className="pt-4 font-semibold text-justify p-2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+              harum minus, fuga esse reiciendis accusantium ad quam molestias
+              dignissimos soluta? Quasi soluta nulla quam sint expedita maxime
+              aliquid quo aut, similique
+            </p>
+
+            <h1 className=" text-[800%] leading-none font-bold ">01</h1>
+          </div>
+        </div>
+      </section>
+
       <section className="section-one">
         <div className="relative writeup ">
           <p className="absolute -top-3 left-0 bg-background px-3 font-bold ">
