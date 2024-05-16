01785c35c57b82be475028b44a170dec99e767d7
Felixx-jnr <uyuoukoh@gmail.com>
2024-05-16 14:31:24 +0100

    review setup

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Slider = () => {
   return (
     <div className="reviews relative bg-slate-950 flex">
-      <div className="reviews-slide flex flex-grow-0">
+      <div className="reviews-slide flex">
         <div className="mx-8 w-40 h-40 bg-orange-400"></div>
         <div className="mx-8 w-40 h-40 bg-cyan-400"></div>
         <div className="mx-8 w-40 h-40 bg-pink-400"></div>
@@ -11,7 +11,7 @@ const Slider = () => {
         <div className="mx-8 w-40 h-40 bg-green-400"></div>
         <div className="mx-8 w-40 h-40 bg-yellow-400"></div>
       </div>
-      <div className="reviews-slide flex flex-grow-0 ">
+      <div className="reviews-slide flex">
         <div className="mx-8 w-40 h-40 bg-orange-400"></div>
         <div className="mx-8 w-40 h-40 bg-cyan-400"></div>
         <div className="mx-8 w-40 h-40 bg-pink-400"></div>
